a17ea889871d5093331818e23c3bce9062390306
zwj <2309840734@qq.com>
2024-03-28 01:01:03 +0800

    feat:Lottie 动画

--- src/views/NotFound/index.vue
+++ src/views/NotFound/index.vue
@@ -1,7 +1,20 @@
 <template>
-  <h1>NotFound Page</h1>
+  <div class="w-screen h-screen flex items-center justify-center">
+    <div class="w-80 h-80" ref="animationRef"></div>
+  </div>
 </template>
-
-<script setup></script>
-
-<style lang="less" scoped></style>
+<script setup>
+import { ref, onMounted } from "vue";
+import lottie from "lottie-web";
+import animationData from "@/assets/data/hamster.json";
+const animationRef = ref(null);
+onMounted(() => {
+  lottie.loadAnimation({
+    animationData,
+    loop: true,
+    autoplay: true,
+    renderer: "svg",
+    container: animationRef.value
+  });
+});
+</script>
